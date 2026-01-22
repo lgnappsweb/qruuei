784834fb2c7ba09fdcef6e0af31d57ff2f2a234f
Your Name <lgnappsweb@gmail.com>
2026-01-22 23:48:19 +0000

    preciso que em ecrã menores tenha apenas 3 colunas de botões (_for eleme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         />
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
         {filteredOccurrences.map((occurrence) => (
           <OccurrenceCard key={occurrence.id} occurrence={occurrence} />
         ))}
