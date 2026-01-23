0cfc67268c8fb715799209d42c16164f61046fa4
Apps Web <lgnappsweb@gmail.com>
2026-01-23 23:34:31 +0000

    coloque aqui somente as prs que poderia ser usada em um acidente coloque

--- src/app/ocorrencias/qud-operacao/page.tsx
+++ src/app/ocorrencias/qud-operacao/page.tsx
@@ -48,17 +48,20 @@ import {
 
 const auxilios = [
   { id: 'PR01', label: 'PR01 - Atendimento inicial' },
-  { id: 'PR07', label: 'PR07 - Escolta' },
-  { id: 'PR09', label: 'PR09 - Outros' },
+  { id: 'PR11', label: 'PR11 - Remoção de vítima para hospital' },
+  { id: 'PR12', label: 'PR12 - Sinalização final de fila' },
   { id: 'PR13', label: 'PR13 - Canalização/Sinalização' },
-  { id: 'PR17', label: 'PR17 - Orientação/Informação ao usuário' },
-  { id: 'PR18', label: 'PR18 - Recusa de dados' },
-  { id: 'PR19', label: 'PR19 - Operação comboio' },
+  { id: 'PR22', label: 'PR22 - Limpeza de pista' },
+  { id: 'PR26', label: 'PR26 - Transferência de carga' },
+  { id: 'PR27', label: 'PR27 - Remoção de veículo' },
+  { id: 'PR31', label: 'PR31 - Remoção de vítima das ferragens' },
+  { id: 'PR32', label: 'PR32 - Destombamento de veículo' },
+  { id: 'PR37', label: 'PR37 - Implantação de Pare e Siga/ Interdição total' },
   { id: 'PR44', label: 'PR44 - Acionamento de Polícia' },
-  { id: 'PR49', label: 'PR49 - Evento acompanhamento pelo CFTV' },
+  { id: 'PR50', label: 'PR50 - Remoção de vítima para P.S' },
   { id: 'PR51', label: 'PR51 - Efetuado Registro Fotográfico' },
-  { id: 'PR53', label: 'PR53 - Meios próprios' },
-  { id: 'PR54', label: 'PR54 - Aux. com ferram./ Empréstimo ferram.' },
+  { id: 'PR58', label: 'PR58 - Avaliação da Vítima' },
+  { id: 'PR61', label: 'PR61 - Abordagem de vítima' },
 ] as const;
 
 const tiposPane = [
