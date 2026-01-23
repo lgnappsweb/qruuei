fc91fcd57da5d7acc2d1e82ca8517d6bcf0094ca
Your Name <lgnappsweb@gmail.com>
2026-01-23 13:46:41 +0000

    aqui dentro do to07 quero que cria o formulario com o mesmo padra dos ou

--- src/components/occurrence-card.tsx
+++ src/components/occurrence-card.tsx
@@ -28,6 +28,8 @@ export function OccurrenceCard({ occurrence }: OccurrenceCardProps) {
       router.push('/ocorrencias/to05');
     } else if (occurrence.id === '9') { // This is TO 06
       router.push('/ocorrencias/to06');
+    } else if (occurrence.id === '10') { // This is TO 07
+      router.push('/ocorrencias/to07');
     } else {
       // TODO: Futuramente, este clique poderá buscar e exibir
       // um relatório detalhado do Firestore e salvar a ocorrência.
