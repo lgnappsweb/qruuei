dec92de3aa880a938b89a3012287b2defe17d8f1
Your Name <lgnappsweb@gmail.com>
2026-01-23 00:23:50 +0000

    TROQUE O ICONE DO QUD RESGATE POR:Ambulance (_for element <LifeBuoy>_)

--- src/lib/occurrences.ts
+++ src/lib/occurrences.ts
@@ -13,7 +13,7 @@ export type Occurrence = {
 };
 
 export const occurrences: Occurrence[] = [
-  { id: '1', title: 'QUD RESGATE', icon: LifeBuoy, color: 'bg-rose-500' },
+  { id: '1', title: 'QUD RESGATE', icon: Ambulance, color: 'bg-rose-500' },
   { id: '2', title: 'QUD OPERAÇÃO', icon: Megaphone, color: 'bg-blue-500' },
   { id: '3', title: 'TRAÇADO DE PISTA', icon: Milestone, color: 'bg-green-500' },
   { id: '4', title: 'TO 01', icon: Truck, color: 'bg-yellow-500' },
