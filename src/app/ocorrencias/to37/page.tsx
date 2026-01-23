5c2f69bd55e7c5db3de9cd7af18356069c1faf32
Apps Web <lgnappsweb@gmail.com>
2026-01-23 19:49:10 +0000

    retira faixa de tolamento e em baixo acrescenta:Rotatória, Trevo, Praça

--- src/app/ocorrencias/to37/page.tsx
+++ src/app/ocorrencias/to37/page.tsx
@@ -208,9 +208,12 @@ export default function OcorrenciaTO37Page() {
                         </SelectTrigger>
                       </FormControl>
                       <SelectContent>
-                        <SelectItem value="Faixa de Rolamento">Faixa de Rolamento</SelectItem>
                         <SelectItem value="Acostamento">Acostamento</SelectItem>
                         <SelectItem value="Área de Domínio">Área de Domínio</SelectItem>
+                        <SelectItem value="Rotatória">Rotatória</SelectItem>
+                        <SelectItem value="Trevo">Trevo</SelectItem>
+                        <SelectItem value="Praça de Pedágio">Praça de Pedágio</SelectItem>
+                        <SelectItem value="BSO">BSO</SelectItem>
                       </SelectContent>
                     </Select>
                     <FormMessage />
