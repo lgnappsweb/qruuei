1305a90e2a380502b53dbe85ff179cc65b927526
Your Name <lgnappsweb@gmail.com>
2026-01-23 15:02:00 +0000

    em ecrã menores deixe o campo de selecionar a hora menor dentro do card

--- src/app/ocorrencias/to09/page.tsx
+++ src/app/ocorrencias/to09/page.tsx
@@ -289,7 +289,7 @@ export default function OcorrenciaTO09Page() {
                     )}
                 />
               </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
                  <FormField
                     control={form.control}
                     name="qtrInicio"
