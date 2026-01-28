fe09cc9c858984634e47a73a9bc9612e94d286f2
Apps Web <lgnappsweb@gmail.com>
2026-01-28 11:36:27 +0000

    COLOQUE NA ORDEM DE MS-112 BR-158 MS-306 E BR436 (_for element <Accordio

--- src/lib/kmz-links.ts
+++ src/lib/kmz-links.ts
@@ -1,15 +1,15 @@
 export const mapsMeLinks = [
   {
+    title: 'MAPS.ME - MS-112',
+    href: 'https://github.com/flavio-dino/maps/raw/main/ms112.kmz',
+  },
+  {
     title: 'MAPS.ME - BR-158',
     href: 'https://github.com/flavio-dino/maps/raw/main/br158.kmz',
   },
   {
     title: 'MAPS.ME - MS-306',
     href: 'https://github.com/flavio-dino/maps/raw/main/ms306.kmz',
-  },
-  {
-    title: 'MAPS.ME - MS-112',
-    href: 'https://github.com/flavio-dino/maps/raw/main/ms112.kmz',
   },
    {
     title: 'MAPS.ME - BR-436',
